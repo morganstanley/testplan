d0abfcfbd18a1afd77577a6e59d49d0030907a1e
yitaor <yitaor@users.noreply.github.com>
2018-11-13 15:10:45 +0800

    Multi-thread testcase execution
    * In a test suite, testcases that do not affect each other could be
      put in the same execution group, and then they can run in parallel
      with multi-threading. Parametrized testcase also supported.
    
    * Better to create parent report for parametrized testcase just before
      executing it, which means wait for all testcaes in previous execution
      group finished, thus, in generated report the testcases are listed in
      the order of execution. In multi-threading testcase execution we will
      remote the testcase report if the testcases have no chance to run.
    
    * Fix a bug that if setup() fails in a suite, the status of suite will
      not be logged even 'display_suite' in 'stdout style' is set True.
      Should put log_suite_status() directly after _run_suite().

--- test/unit/testplan/testing/multitest/test_suite.py
+++ test/unit/testplan/testing/multitest/test_suite.py
@@ -59,6 +59,28 @@ class MySuite3(object):
         pass
 
 
+@testsuite
+class MySuite4(object):
+    @testcase(execution_group='group_0')
+    def case1(self, env, result):
+        pass
+
+    @testcase(execution_group='group_1')
+    def case2(self, env, result):
+        pass
+
+    @testcase(execution_group='group_0')
+    def case3(self, env, result):
+        pass
+
+    @testcase(execution_group='group_1')
+    def case4(self, env, result):
+        pass
+
+    @testcase(parameters=(1, 2, 3), execution_group='group_parallel')
+    def case(self, env, result, param):
+        pass
+
 def test_basic_suites():
     mysuite = MySuite1()
 
@@ -69,16 +91,6 @@ def test_basic_suites():
     for method in get_testcase_methods(MySuite1):
         assert method.__name__ in cases
         assert callable(method)
-
-    for method in mysuite.get_testcases():
-        assert method.__name__ in cases
-        assert callable(method)
-
-
-def test_basic_parametrization():
-    mysuite = MySuite3()
-    cases = ('case__param_1', 'case__param_2', 'case__param_3')
-    assert tuple(mysuite.__testcases__) == cases
 
     for method in mysuite.get_testcases():
         assert method.__name__ in cases
@@ -98,6 +110,26 @@ def test_basic_suite_tags():
         assert method.__tags__ == case_dict[method.__name__]
         assert method.__tags_index__ == tagging.merge_tag_dicts(
             case_dict[method.__name__], mysuite.__tags__)
+
+
+def test_basic_parametrization():
+    mysuite = MySuite3()
+    cases = ('case__param_1', 'case__param_2', 'case__param_3')
+    assert tuple(mysuite.__testcases__) == cases
+
+    for method in mysuite.get_testcases():
+        assert method.__name__ in cases
+        assert callable(method)
+
+
+def test_basic_execution_group():
+    mysuite = MySuite4()
+
+    for i, method in enumerate(mysuite.get_testcases()):
+        if method.__name__.startswith('case__'):
+            assert method.execution_group == 'group_parallel'
+        else:
+            assert method.execution_group == 'group_{}'.format(i%2)
 
 
 def incorrect_case_signature1():
