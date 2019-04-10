c10352d40af16e5096c60109223b3de35613c06f
ryan-collingham <43373633ryan-collingham@users.noreply.github.com>
2019-04-10 16:34:04 +0100

    Fix up examples test to work with new tests/ dir

--- tests/functional/examples/test_examples.py
+++ tests/functional/examples/test_examples.py
@@ -11,16 +11,22 @@ import platform
 
 FILE_DIR = os.path.dirname(__file__)
 
+# This file is under tests/functional/examples, so the root of this repo
+# should be 3 levels up.
+ROOT_DIR = os.path.abspath(os.path.join(
+    FILE_DIR, *(os.pardir for _ in range(3))))
+EXAMPLES_ROOT = os.path.join(ROOT_DIR, 'examples')
+
 ON_WINDOWS = platform.system() == 'Windows'
 
 KNOWN_EXCEPTIONS = [
-    "TclError: Can't find a usable init\.tcl in the following directories:", # Matplotlib module improperly installed. Will skip Data Science example.
-    "ImportError: lib.*\.so\..+: cannot open shared object file: No such file or directory", # Matplotlib module improperly installed. Will skip Data Science example.
+    r"TclError: Can't find a usable init\.tcl in the following directories:", # Matplotlib module improperly installed. Will skip Data Science example.
+    r"ImportError: lib.*\.so\..+: cannot open shared object file: No such file or directory", # Matplotlib module improperly installed. Will skip Data Science example.
     "ImportError: No module named sklearn.*", # Missing module sklearn. Will skip Data Science example.
     "ImportError: No module named Tkinter", # Missing module Tkinter. Will skip Data Science example.
     "ImportError: No module named _tkinter.*", # Missing module Tkinter. Will skip Data Science example.
     "RuntimeError: Download pyfixmsg library .*", # Missing module pyfixmsg. Will skip FIX example.
-    "No spec file set\. You should download .*", # Missing FIX spec file. Will skip FIX example.
+    r"No spec file set\. You should download .*", # Missing FIX spec file. Will skip FIX example.
     "AttributeError: 'module' object has no attribute 'poll'",
     "RuntimeError: You need to compile test binary first.", # Need to compile cpp binary first. Will skip GTest example.
     "FATAL ERROR: Network error: Connection refused", # We don't fail a pool test for connection incapability.
@@ -52,33 +58,12 @@ def _param_formatter(param):
     return repr(param)
 
 
-def _examples_root():
-    """
-    Find the examples directory that sits next to test & testplan
-    """
-
-    cwd = FILE_DIR
-    while True:
-        contents = os.listdir(cwd)
-        if all([entry in contents for entry in ROOT_DIR_CONTENTS]):
-            break
-        parent_dir = os.path.dirname(cwd)
-        if os.path.realpath(cwd) == os.path.realpath(parent_dir):
-            raise RuntimeError('Could not find repo directory')
-        cwd = parent_dir
-
-    return os.path.abspath(os.path.join(
-        cwd,
-        'examples'
-    ))
-
-
 @pytest.mark.parametrize(
     'root,filename',
     [
         (os.path.abspath(root), filename)
         for root, _, files in os.walk(
-            _examples_root(), followlinks=True)
+            EXAMPLES_ROOT, followlinks=True)
         for filename in files
         if ('test_plan' in filename and filename.endswith('.py'))
     ],
