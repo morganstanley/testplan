2f7bd3e5eb69eda6df3e7e98fd6d557b4b0518a1
ryan-collingham <43373633+ryan-collingham@users.noreply.github.com>
2019-08-06 16:53:43 +0100

    Update example to also attach an image

--- examples/Attachments/test_plan.py
+++ examples/Attachments/test_plan.py
@@ -4,6 +4,8 @@ import tempfile
 import os
 import sys
 
+from PIL import Image, ImageDraw
+
 import testplan
 from testplan.testing import multitest
 
@@ -36,6 +38,34 @@ class TestSuite(object):
         """
         result.attach(self.tmpfile,
                       description="Attaching the same text file again")
+
+    @multitest.testcase
+    def test_attach_img(self, env, result):
+        """
+        Attach an image instead of a plain text file. We draw an image and
+        save it as a .png using the Pillow library.
+        """
+        canvas = (400, 300)
+
+        # Rectangles (width, height, left position, top position)
+        frames = [(50, 50, 5, 5), (60, 60, 100, 50), (100, 100, 205, 120)]
+
+        # Init canvas
+        im = Image.new('RGBA', canvas, (255, 255, 255, 255))
+        draw = ImageDraw.Draw(im)
+
+        # Draw rectangles
+        for frame in frames:
+            x1, y1 = frame[2], frame[3]
+            x2, y2 = frame[2] + frame[0], frame[3] + frame[1]
+            draw.rectangle([x1, y1, x2, y2], outline=(0, 0, 0, 255))
+
+        # Save image
+        img_path = os.path.join(env.runpath, "image.png")
+        im.save(img_path)
+
+        # Attach to report.
+        result.attach(img_path, description="I drew some rectangles")
 
 
 @testplan.test_plan(name="AttachmentPlan")
