72cd29a6c103a54d4a4df4537868ed3b99dd6d99
ryan-collingham <43373633+ryan-collingham@users.noreply.github.com>
2019-08-29 16:53:58 +0000

    Fix race condition in parallel MTest example (#252)
    * Fix race condition in parallel MTest example
    
    Should not rely on time.sleep for synchronisation - use Barrier and
    Event primitives instead. Add a Lock to protect the shared refcounts
    counter inside the ExclusiveResourceManager driver.
    
    * Update example to clarify testcase ordering
    
    * Describe usages of synchronisation primitives

--- examples/Multitest/Parallel/parallel_tasks.py
+++ examples/Multitest/Parallel/parallel_tasks.py
@@ -1,9 +1,12 @@
 """Example test suite to demonstrate grouped parallel MultiTest execution."""
-import time
+import threading
 
 from testplan.testing.multitest import MultiTest
 from testplan.testing.multitest.suite import testsuite, testcase
+from testplan.common.utils import thread as thread_utils
+
 import resource_manager
+
 
 @testsuite
 class SampleTest(object):
@@ -16,19 +19,40 @@ class SampleTest(object):
 
     You will find that modifying a single test from the "first" group to acquire
     the "second" resource (or vice-versa) will cause the test to fail.
+
+    NOTE: when running a parallel MultiTest, all testcases from a given
+    execution group are run together, regardless of the order they are defined
+    within the testsuite class. Each execution group is run separately from all
+    others. This is in contrast to the default serial mode, where testcases
+    are run serially in the order they are defined within the testsuite class.
     """
 
     def __init__(self):
-        self._test_g2_1_done = False
+        # A Barrier is a synchronisation primitive which allows a fixed number
+        # of threads (in our case, 2) to wait for each other. We use it here
+        # to demonstrate that testcases are run concurrently and how they may
+        # be synchronised with each other.
+        #
+        # Note that on Python 3 you can use the Barrier class from the standard
+        # library:
+        # https://docs.python.org/3.7/library/threading.html#barrier-objects .
+        # Here we use a backported Barrier provided by Testplan, which works
+        # on both Python 2 and 3.
+        self._barrier = thread_utils.Barrier(2)
+
+        # The Event synchronisation primitive allows one thread to signal to
+        # another that is waiting on the first thread to do some work. We use
+        # it here to demonstrate another way testcases within the same
+        # execution group may be synchronised with each other.
+        self._test_g2_1_done = threading.Event()
 
     @testcase(execution_group='first')
     def test_g1_1(self, env, result):
-        """Wait for test_g1_2 to also acquire the first resource."""
-        with env.resources['first'] as res:
-            result.true(res.active)
-            while res.refcount < 2:
-                result.log('Waiting for test_g1_2...')
-                time.sleep(1)
+        """
+        Wait for test_g1_2 to also acquire the first resource. Assert that the
+        refcount is 2.
+        """
+        self._test_g1_impl(env, result)
 
     @testcase(execution_group='second')
     def test_g2_1(self, env, result):
@@ -36,33 +60,44 @@ class SampleTest(object):
         with env.resources['second'] as res:
             result.true(res.active)
             result.equal(res.refcount, 1)
-        self._test_g2_1_done = True
+        self._test_g2_1_done.set()
 
     @testcase(execution_group='first')
     def test_g1_2(self, env, result):
         """
-        Sleep before acquiring the first resource. Wait for test_g1_1 to
-        release it first.
+        Mirror image of test_g1_1. We wait for test_g1_1 to acquire the first
+        resource while running in another thread, then assert that the refcount
+        is 2.
         """
-        time.sleep(2.5)
-
-        with env.resources['first'] as res:
-            result.true(res.active)
-            result.equal(res.refcount, 2)
-            while res.refcount == 2:
-                result.log('Waiting for test_g1_1...')
-                time.sleep(1)
+        self._test_g1_impl(env, result)
 
     @testcase(execution_group='second')
     def test_g2_2(self, env, result):
         """Wait for test_g2_1 to release the resource before acquiring it."""
-        while not self._test_g2_1_done:
-            result.log('Waiting for test_g2_1 to finish...')
-            time.sleep(1)
+        self._test_g2_1_done.wait()
 
         with env.resources['second'] as res:
             result.true(res.active)
             result.equal(res.refcount, 1)
+
+    def _test_g1_impl(self, env, result):
+        """
+        Implementation of test_g1 testcases. Both testcases use the same logic
+        but are run concurrently in separate threads.
+        """
+        with env.resources['first'] as res:
+            result.true(res.active)
+
+            # Wait for both threads to acquire the resource.
+            self._barrier.wait()
+
+            # Both threads have acquired the resource - check that the refcount
+            # is 2.
+            result.equal(res.refcount, 2)
+
+            # Wait for both threads to check the refcount before releasing the
+            # resource.
+            self._barrier.wait()
 
 
 def make_multitest():
