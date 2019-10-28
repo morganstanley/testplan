84cd289d2f8f4c19211acdeb7e9ddfd6e6fb0df7
ryan-collingham <43373633+ryan-collingham@users.noreply.github.com>
2019-10-28 11:51:51 +0000

    Interactive examples modifications
    - Modify the basic example to recommend viewing the Swagger UI schema
    - Remove other examples from docs temporarily until they are fixed to
      work with the new REST API.

--- examples/Interactive/Basic/test_plan.py
+++ examples/Interactive/Basic/test_plan.py
@@ -10,7 +10,7 @@ from my_tests.mtest import make_multitest
 
 # Hard coding interactive mode usage.
 @test_plan(name='MyPlan',
-           interactive=True,
+           interactive_port=0,
            stdout_style=Style(
                passing=StyleEnum.ASSERTION_DETAIL,
                failing=StyleEnum.ASSERTION_DETAIL))
@@ -28,26 +28,7 @@ if __name__ == '__main__':
 # INTERACTIVE MODE DEMO:
 # ----------------------
 #
-# When HTTP handler starts listening on <IP>:$PORT
-# use a tool like curl to send HTTP requests and execute/reload tests.
-#
-# First execute the tests:
-#     curl -X POST http://127.0.0.1:$PORT/sync/run_tests
-#
-# Make an an edit in my_tests/dependency.py
-#   VALUE = 3
-#     change to:
-#   VALUE = 1
-#
-# Reload the code:
-#     curl -X POST http://127.0.0.1:$PORT/sync/reload
-#
-# Re-run the tests:
-#     curl -X POST http://127.0.0.1:$PORT/sync/run_tests
-#
-# Run only one suite:
-#     curl -X POST http://127.0.0.1:$PORT/sync/run_test_suite -d '{"test_uid": "Test1", "suite_uid": "BasicSuite"}'
-#
-# .. and all other operations that Testplan interactive provides.
-
+# View the API schema at either the localhost or LAN address printed when this
+# testplan script is run. The schema is interactive so can be used to try out
+# the API. To run a test, set its status to "running" on a PUT update.
 
