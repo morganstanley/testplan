1a59c0cbb29c2f9584f5f9aacc21e24c6bd3a7d3
David C. Morse <David.Morse@morganstanley.com>
2020-03-06 17:06:26 +0000

    require trailing slashes on URLs

--- testplan/web_ui/testing/src/index.js
+++ testplan/web_ui/testing/src/index.js
@@ -8,16 +8,29 @@ import Home from './Common/Home';
 
 // import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 /**
  * This single App provides multiple functions controlled via the URL path
  * accessed. We are using React-Router to control which type of report is
  * rendered and to extract the report UID from the URL when necessary.
+ *
+ * We use "strict" here to ensure that paths end with a trailing slash. This
+ * makes the UI-rendered hash routes more distinguishable from the
+ * server-rendered regular routes. The final <Route> catches URLs entered by
+ * the user that don't contain a trailing slash, e.g. "/:url", and redirects
+ * them to e.g. "/:url/".
  */
 const AppRouter = () => (
-  <Router>
+  <BrowserRouter>
     <Switch>
+      {/* Must be first - require trailing slash */}
+      <Route strict exact
+             from="(|.*?[^/])"
+             component={props => (
+                 <Redirect to={`${props.location.pathname}/`}/>
+             )}
+      />
       <Route exact path="/" component={Home} />
       <Route path="/testplan/:uid" component={BatchReport} />
       <Route path="/interactive/_dev">
@@ -26,9 +39,10 @@ const AppRouter = () => (
       <Route path="/interactive">
         <InteractiveReport dev={false} poll_ms={POLL_MS} />
       </Route>
+      {/* Must be last */}
       <Route component={EmptyReport} />
     </Switch>
-  </Router>
+  </BrowserRouter>
 );
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
