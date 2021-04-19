e2581f6733856220f41e9b6a9238b28050cf8518
yuxuan-ms <43603130+yuxuan-ms@users.noreply.github.com>
2021-04-19 15:20:02 +0800

    Add dropdown menu in breadscrumb bar

--- testplan/web_ui/testing/src/Common/Styles.js
+++ testplan/web_ui/testing/src/Common/Styles.js
@@ -2,17 +2,40 @@
  * Common aphrodite styles.
  */
 import {StyleSheet} from 'aphrodite';
+import {
+  RED,
+  GREEN,
+  ORANGE,
+  BLACK,
+} from "../Common/defaults";
+
+export const unselectable = {
+  "moz-user-select": "-moz-none",
+  "khtml-user-select": "none",
+  "webkit-user-select": "none",
+  "ms-user-select": "none",
+  "user-select": "none",
+};
 
 
-const Styles = StyleSheet.create({
-  unselectable: {
-    "moz-user-select": "-moz-none",
-    "khtml-user-select": "none",
-    "webkit-user-select": "none",
-    "ms-user-select": "none",
-    "user-select": "none",
+export const statusStyles = {
+  passed: {
+    color: GREEN,
   },
+  failed: {
+    color: RED,
+  },
+  error: {
+    color: RED,
+  },
+  unstable: {
+    color: ORANGE,
+  },
+  unknown: {
+    color: BLACK,
+  },
+};
+
+export default StyleSheet.create({
+  unselectable: unselectable
 });
-
-export default Styles;
-
