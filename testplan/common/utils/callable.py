76d7b66ac258a96a1ae72897f0b42258972d075a
Pyifan <Pyifan@users.noreply.github.com>
2021-09-24 15:45:52 +0800

    Make custom_wrapper apply for non-parametrized case.

--- testplan/common/utils/callable.py
+++ testplan/common/utils/callable.py
@@ -7,7 +7,6 @@ from collections import namedtuple
 WRAPPER_ASSIGNMENTS = functools.WRAPPER_ASSIGNMENTS + (
     "__tags__",
     "__tags_index__",
-    "wrapper_of",
     "summarize",
     "summarize_num_passing",
     "summarize_num_failing",
@@ -72,70 +71,20 @@ def getargspec(callable_):
     )
 
 
-# backport from python 3.6, 2.7 version does not catch AttributeError
-def update_wrapper(
-    wrapper,
-    wrapped,
-    assigned=WRAPPER_ASSIGNMENTS,
-    updated=functools.WRAPPER_UPDATES,
-):
-    """
-    Update a wrapper function to look like the wrapped function.
-
-    :param wrapper: Function to be updated.
-    :type wrapper: ``func``
-    :param wrapped: Original function.
-    :type wrapped: ``func``
-    :param assigned: Tuple naming the attributes assigned directly from the
-                     wrapped function to the wrapper function (defaults to
-                     functools.WRAPPER_ASSIGNMENTS)
-    :type assigned: ``tuple``
-    :param updated: tuple naming the attributes of the wrapper that are updated
-                    with the corresponding attribute from the wrapped function
-                    (defaults to functools.WRAPPER_UPDATES)
-    :type updated: ``tuple``
-    :return: Wrapper function.
-    :rtype: ``func``
-    """
-    for attr in assigned:
-        try:
-            value = getattr(wrapped, attr)
-        except AttributeError:
-            pass
-        else:
-            setattr(wrapper, attr, value)
-    for attr in updated:
-        getattr(wrapper, attr).update(getattr(wrapped, attr, {}))
-    # Issue #17482: set __wrapped__ last so we don't inadvertently copy it
-    # from the wrapped function when updating __dict__
-    wrapper.__wrapped__ = wrapped
-    # Return the wrapper so this can be used as a decorator via partial()
-    return wrapper
-
-
 def wraps(
     wrapped, assigned=WRAPPER_ASSIGNMENTS, updated=functools.WRAPPER_UPDATES
 ):
     """
-    Custom wraps function that uses the backported ``update_wrapper``.
-
-    Also sets ``wrapper_of`` attribute for code highlighting, for methods that
-    are decorated for the first time.
+    Custom wraps function that copies some additional attr than default.
     """
 
     def _inner(wrapper):
-        wrapper = update_wrapper(
+        wrapper = functools.update_wrapper(
             wrapper=wrapper,
             wrapped=wrapped,
             assigned=assigned,
             updated=updated,
         )
-
-        # When a method is decorated for the first time it will not have
-        # `wrapper_of` attribute set, so `update_wrapper` won't be able to copy
-        # it over. That's why we have to explicitly assign it here.
-        if not hasattr(wrapped, "wrapper_of"):
-            wrapper.wrapper_of = wrapped
         return wrapper
 
     return _inner
