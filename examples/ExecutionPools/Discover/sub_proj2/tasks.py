f25e1a9b2a0e58a86b7fc42b9aaaff8b8d5d4a5f
raoyitao <raoyitao@users.noreply.github.com>
2021-12-08 19:15:07 +0800

    `task_target` can be used without parentheses
    * Allow the decorator `task_target` used without parentheses following.
    * When `schedule_all` is used to discover task targets, once a target
      is found then should be added to executor, rather adding all targets
      after directory traversed, so the log of 'Discovered task target' and
      'Task created' can match for easy tracing, also the exception can be
      raised in advance if there's Test instances with duplicate name.
    * Update document and examples.

--- examples/ExecutionPools/Discover/sub_proj2/tasks.py
+++ examples/ExecutionPools/Discover/sub_proj2/tasks.py
@@ -4,14 +4,14 @@ from testplan.testing.multitest import MultiTest
 from sub_proj2.suites import Suite1, Suite2
 
 
-@task_target()
+@task_target
 def make_multitest1():
     # a test target shall only return 1 runnable object
     test = MultiTest(name="Proj2-Suite1", suites=[Suite1()])
     return test
 
 
-@task_target()
+@task_target
 def make_multitest2():
     # a test target shall only return 1 runnable object
     test = MultiTest(name="Proj2-Suite2", suites=[Suite2()])
