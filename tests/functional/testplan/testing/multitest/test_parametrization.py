91959ceace33cfd500f95a6630b0f2bd5879453e
nober97 <86246924+nober97@users.noreply.github.com>
2021-12-10 17:14:56 +0100

    Add grouped time info to parametrized testcases (#734)
    * Add grouped time info to parametrized testcases
    
    * Optimize the sorting of parametrized testcases from non-parametrized ones
    
    * Add timing info to parametrized testcases (ran parallely and serially)
    
    * Add comment and security checks, remove unused _run_parametrized_testcases method
    
    * Add test
    
    * Introduction of _add_runtime_info function (DRY)
    
    * Simplify min-max search in _add_runtime_info function and remove unnecessary return value
    
    * Update announcement
    
    * Correct announcement
    
    * Min-max search made more compact
    
    Co-authored-by: Pyifan <37059868+Pyifan@users.noreply.github.com>

--- tests/functional/testplan/testing/multitest/test_parametrization.py
+++ tests/functional/testplan/testing/multitest/test_parametrization.py
@@ -19,6 +19,7 @@ from testplan.report import (
     ReportCategories,
 )
 from testplan.common.utils.testing import check_report, warnings_suppressed
+from testplan.common.utils.timing import Interval
 
 LOGGER = logging.getLogger()
 
@@ -679,3 +680,40 @@ def test_order_of_parametrization_report(mockplan):
     ]
 
     check_parametrization(mockplan, MySuite, report_entries)
+
+
+def test_timing_info_of_parametrized_group_report(mockplan):
+    @testsuite
+    class SampleTest(object):
+        @testcase(
+            parameters=(
+                (5, 5, 10),
+                (3, 2, 5),
+                (0, 0, 0),
+                ("foo", "bar", "foobar"),
+            )
+        )
+        def addition(self, env, result, a, b, expected):
+            result.equal(a + b, expected)
+
+        @testcase(parameters=(2, 4, 6, 8), execution_group="is even")
+        def is_even(self, env, result, value):
+            result.equal(value % 2, 0)
+
+    multitest = MultiTest(name="Primary", suites=[SampleTest()])
+
+    mockplan.add(multitest)
+    mockplan.run()
+
+    serial_group_report_timer = (
+        mockplan.report.entries[0].entries[0].entries[0].timer
+    )
+    parallel_group_report_timer = (
+        mockplan.report.entries[0].entries[0].entries[1].timer
+    )
+
+    assert "run" in serial_group_report_timer.keys()
+    assert "run" in parallel_group_report_timer.keys()
+
+    assert isinstance(serial_group_report_timer["run"], Interval)
+    assert isinstance(parallel_group_report_timer["run"], Interval)
