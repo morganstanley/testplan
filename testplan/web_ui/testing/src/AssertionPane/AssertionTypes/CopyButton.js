245884b3edf0851ded7e61879bba52c90f75852c
MartonSzikszai <56308393+m6AI@users.noreply.github.com>
2022-02-07 14:56:52 +0100

    Migrate to NPM and add package-lock.
    * Generated package-lock.json using NPM.
    
    * Updated react-test-renderer version to support useState hook with shallow from enzyme.
    
    * Listed package-lock.json as a gitignore exception.
    
    * Removed react-copy-html-to-clipboard as dependency and re-implemented its used feature locally.

--- testplan/web_ui/testing/src/AssertionPane/AssertionTypes/CopyButton.js
+++ testplan/web_ui/testing/src/AssertionPane/AssertionTypes/CopyButton.js
@@ -1,7 +1,77 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {Button} from 'reactstrap';
-import CopyToClipboard from 'react-copy-html-to-clipboard';
+import copy from 'copy-html-to-clipboard';
+
+class CopyToClipboard extends React.PureComponent {
+  static propTypes = {
+    text: PropTypes.any.isRequired,
+    children: PropTypes.element.isRequired,
+    onCopy: PropTypes.func,
+    options: PropTypes.shape({
+      debug: PropTypes.bool,
+      message: PropTypes.string,
+      asHtml: PropTypes.bool,
+      onlyHtml: PropTypes.bool,
+      canUsePrompt: PropTypes.bool,
+    })
+  };
+
+
+  static defaultProps = {
+    onCopy: undefined,
+    options: undefined
+  };
+
+
+  onClick = event => {
+    const {
+      text,
+      onCopy,
+      children,
+      options
+    } = this.props;
+
+    const elem = React.Children.only(children);
+
+    let textToCopy;
+    if (typeof text == 'function')
+        textToCopy = text();
+    else
+        textToCopy = text;
+
+    const result = copy(textToCopy, options);
+
+    if (onCopy) {
+      onCopy(textToCopy, result);
+    }
+
+    if (elem && elem.props) {
+      if (typeof elem.props.onClick === 'function') {
+        elem.props.onClick(event);
+      } else if (typeof elem.props.onTouchTap === 'function') {
+        elem.props.onTouchTap(event);
+      }
+    }
+    
+  };
+
+
+  render() {
+    const {
+      text: _text,
+      onCopy: _onCopy,
+      options: _options,
+      children,
+      ...props
+    } = this.props;
+    const elem = React.Children.only(children);
+
+    return React.cloneElement(
+      elem, {...props, onClick: this.onClick, onTouchTap: this.onClick}
+    );
+  }
+}
 
 /**
  * Component that renders the buttons of copy html to clipboard.
