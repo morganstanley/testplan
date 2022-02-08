721fd2741e90a92ab19a61e7e18498da80c4b7e7
MartonSzikszai <56308393+m6AI@users.noreply.github.com>
2022-02-08 09:14:13 +0100

    Migrate to NPM and add package-lock.
    * Replacing uses of Yarn with NPM across the code.
    
    * Gardening.

--- tests/unit/testplan/web_ui/testing/test_jest.py
+++ tests/unit/testplan/web_ui/testing/test_jest.py
@@ -1,22 +1,26 @@
-"""Run tests for the UI code."""
-import subprocess
+"""
+Tests for the UI code.
+"""
 import os
+import subprocess
 
 import pytest
+from pytest_test_filters import skip_on_windows
 
 from testplan import web_ui
-from pytest_test_filters import skip_on_windows
 
 TESTPLAN_UI_DIR = os.path.abspath(
     os.path.join(os.path.dirname(web_ui.__file__), "testing")
 )
 
 
-def yarn_installed():
-    """Check if npm has been installed."""
+def is_npm_installed():
+    """
+    Checks if NPM has been installed.
+    """
     with open(os.devnull, "w") as FNULL:
         try:
-            subprocess.check_call("yarn --version", shell=True, stdout=FNULL)
+            subprocess.check_call("npm --version", shell=True, stdout=FNULL)
         except subprocess.CalledProcessError:
             return False
         else:
@@ -24,30 +28,35 @@ def yarn_installed():
 
 
 def tp_ui_installed():
-    """Check if the Testplan UI dependencies have been installed."""
+    """
+    Checks if the Testplan UI dependencies have been installed.
+    """
     node_modules_dir = os.path.join(TESTPLAN_UI_DIR, "node_modules")
-    print(os.path.abspath(node_modules_dir))
     return os.path.exists(node_modules_dir)
 
 
 @pytest.mark.skipif(
-    not (yarn_installed() and tp_ui_installed()),
+    not (is_npm_installed() and tp_ui_installed()),
     reason="requires npm & testplan UI to have been installed.",
 )
 def test_testplan_ui():
-    """Run the Jest unit tests for the UI."""
+    """
+    Runs the Jest unit tests for the UI.
+    """
     env = os.environ.copy()
     env["CI"] = "true"
     subprocess.check_call(
-        "yarn test", shell=True, cwd=TESTPLAN_UI_DIR, env=env
+        "npm run test", shell=True, cwd=TESTPLAN_UI_DIR, env=env
     )
 
 
 @skip_on_windows(reason="We run this on linux only")
 @pytest.mark.skipif(
-    not (yarn_installed() and tp_ui_installed()),
-    reason="requires yarn & testplan UI have been installed.",
+    not (is_npm_installed() and tp_ui_installed()),
+    reason="requires NPM & testplan UI have been installed.",
 )
 def test_eslint():
-    """Run eslint over the UI source code."""
-    subprocess.check_call("yarn lint", shell=True, cwd=TESTPLAN_UI_DIR)
+    """
+    Runs eslint over the UI source code.
+    """
+    subprocess.check_call("npm run lint", shell=True, cwd=TESTPLAN_UI_DIR)
