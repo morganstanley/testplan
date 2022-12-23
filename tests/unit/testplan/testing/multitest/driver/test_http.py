42cb39ac3729d51f286828b66cadf44bdac0f813
Robert Nemes <Robert.Nemes@morganstanley.com>
2022-12-23 10:50:56 +0000

    Added Unit test

--- tests/unit/testplan/testing/multitest/driver/test_http.py
+++ tests/unit/testplan/testing/multitest/driver/test_http.py
@@ -94,8 +94,37 @@ class TestHTTP:
         assert "text/plain" == r.headers["content-type"]
         assert text == r.text.strip("\n")
 
+    def test_server_flush(self, http_server):
+        """Test flushing the server request queue."""
+        http_server.flush_request_queue()
+        msg = http_server.receive(timeout=0.5)
+        assert msg is None
+
+    @pytest.mark.parametrize("method", ("put", "post"))
+    def test_client_method_request_with_data(
+        self, method, http_server, http_client
+    ):
+        """Test making HTTP requests from client to server objects."""
+        _URL = "/random/text"
+        _CONTENT_TYPE_KEY = "Content-Type"
+        _CONTENT_TYPE = "application/json"
+
+        method_request = getattr(http_client, method)
+        json_content = {"this": ["is", "a", "json", "object"]}
+        method_request(
+            api=_URL,
+            json=json_content,
+            headers={_CONTENT_TYPE_KEY: _CONTENT_TYPE},
+        )
+
+        r = http_server.receive(timeout=10)
+
+        assert _CONTENT_TYPE == r.headers[_CONTENT_TYPE_KEY]
+        assert json_content == r.json
+        assert _URL == r.path_url
+
     def test_client_flush(self, http_client):
-        """Test flushing the client receuve queue."""
+        """Test flushing the client receive queue."""
         http_client.get("random/text")
         http_client.flush()
         msg = http_client.receive(timeout=0.1)
