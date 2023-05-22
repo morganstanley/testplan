c70552d3c564614009fea5cc5affb9d758355916
Krisztian Notaisz <61833595+kn-ms@users.noreply.github.com>
2023-05-22 11:17:01 +0200

    React upgrade (#954)
    * upgrade tot react17
    
    major upgrades:
    - react
    - react-scripts
    - ag-grid
    
    * Fix some rendering issues
    
    - added buffr + polyfill as webpack 5 needs that
    - fixed interactive mode to pass all router props
    - fixed text attachment scrolling
    - adapted marcdown to api changes
    
    * updated jest config
    
    rehype-raw is an esm module but jest expect everything under node_modules is
    cjs so they should not be ignored from transformation, hence the ignore
    pattern need to be updated to leave the esm modules out.
    
    * update newsfragment
    
    ---------
    
    Co-authored-by: Krisztian Notaisz <kn-ms@users.noreply.github.com>

---
+++ testplan/web_ui/testing/src/AssertionPane/AssertionTypes/SyntaxHighlighterSettings.js
@@ -0,0 +1,10 @@
+import style from "react-syntax-highlighter/dist/cjs/styles/prism/one-light";
+
+const defaultSyntax = {
+  style,
+  customStyle: {},
+  showLineNumbers: true,
+  showInlineLineNumbers: false,
+};
+
+export default defaultSyntax;
