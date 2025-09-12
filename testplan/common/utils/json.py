51a5e75bf75d9f6062c18d3d8915ce0ac7c47e3b
zhenyu-ms <111329301+zhenyu-ms@users.noreply.github.com>
2025-09-12 18:44:17 +0800

    refactor since orjson made compulsory

--- testplan/common/utils/json.py
+++ testplan/common/utils/json.py
@@ -1,43 +1,21 @@
-import json
 from pathlib import Path
 from typing import Union
 
-_USE_ORJSON = False
-
-try:
-    import orjson
-except ImportError:
-    pass
-else:
-    _USE_ORJSON = True
+import orjson
 
 
 def json_loads(data: str):
-    if _USE_ORJSON:
-        return orjson.loads(data)
-    else:
-        return json.loads(data)
+    return orjson.loads(data)
 
 
 def json_dumps(data, indent_2=False, default=None) -> str:
-    if _USE_ORJSON:
-        return orjson.dumps(
-            data,
-            default=default,
-            option=(orjson.OPT_INDENT_2 if indent_2 else 0)
-            | orjson.OPT_SERIALIZE_NUMPY
-            | orjson.OPT_NON_STR_KEYS,
-        ).decode()
-    else:
-        if default:
-
-            class _E(json.JSONEncoder):
-                def default(self, o):
-                    return default(o)
-
-        else:
-            _E = None
-        return json.dumps(data, cls=_E, indent=2 if indent_2 else None)
+    return orjson.dumps(
+        data,
+        default=default,
+        option=(orjson.OPT_INDENT_2 if indent_2 else 0)
+        | orjson.OPT_SERIALIZE_NUMPY
+        | orjson.OPT_NON_STR_KEYS,
+    ).decode()
 
 
 def json_load_from_path(path: Union[str, Path]) -> dict:
