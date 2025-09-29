1cc9cf79df2c0609146065c3ea48658b4ac76fd8
zsambokiandras <13022325+zsambokiandras@users.noreply.github.com>
2025-09-29 18:27:02 +0200

    Print search-friendly anchor in testplan log for test/suite/case start/stop (#1232)
    * original changes from PR#1225
    
    * fix MultiTest hook indentation
    
    * refactor testsuite, testcase and pre/post hook logging
    
    * make logs more visible
    
    * add tests

--- testplan/testing/multitest/base.py
+++ testplan/testing/multitest/base.py
@@ -36,6 +36,7 @@ from testplan.testing.multitest.suite import (
     get_testcase_metadata,
 )
 from testplan.testing.multitest.test_metadata import TestMetadata
+from testplan.testing.base import TestLifecycle
 
 
 def iterable_suites(obj):
@@ -325,6 +326,9 @@ class MultiTest(testing_base.Test):
         # if they are marked with an execution group.
         self._thread_pool = None
 
+        self.log_suite_lifecycle = functools.partial(
+            self._log_lifecycle, indent=testing_base.SUITE_INDENT
+        )
         self.log_suite_status = functools.partial(
             self._log_status, indent=testing_base.SUITE_INDENT
         )
@@ -516,6 +520,8 @@ class MultiTest(testing_base.Test):
                         self.cfg.skip_strategy.to_option(),
                     )
                     break
+
+                self.log_suite_lifecycle(testsuite, TestLifecycle.SUITE_END)
 
             style = self.get_stdout_style(report.passed)
             if style.display_test:
@@ -798,6 +804,9 @@ class MultiTest(testing_base.Test):
 
     def _run_suite(self, testsuite, testcases):
         """Runs a testsuite object and returns its report."""
+
+        self.log_suite_lifecycle(testsuite, TestLifecycle.SUITE_START)
+
         _check_testcases(testcases)
         testsuite_report = self._new_testsuite_report(testsuite)
 
@@ -1050,6 +1059,11 @@ class MultiTest(testing_base.Test):
         if not self.active:
             return None
 
+        if method_name == "setup":
+            self.log_suite_lifecycle(testsuite, TestLifecycle.SETUP_START)
+        elif method_name == "teardown":
+            self.log_suite_lifecycle(testsuite, TestLifecycle.TEARDOWN_START)
+
         method_report = self._suite_related_report(method_name)
         case_result = self.cfg.result(
             stdout_style=self.stdout_style,
@@ -1095,6 +1109,11 @@ class MultiTest(testing_base.Test):
         )
         self._xfail(pattern, method_report)
         method_report.runtime_status = RuntimeStatus.FINISHED
+
+        if method_name == "setup":
+            self.log_suite_lifecycle(testsuite, TestLifecycle.SETUP_END)
+        elif method_name == "teardown":
+            self.log_suite_lifecycle(testsuite, TestLifecycle.TEARDOWN_END)
 
         return method_report
 
@@ -1142,6 +1161,8 @@ class MultiTest(testing_base.Test):
     ):
         """Runs a testcase method and returns its report."""
 
+        self.log_testcase_lifecycle(testcase, TestLifecycle.TESTCASE_START)
+
         testcase_report = testcase_report or self._new_testcase_report(
             testcase
         )
@@ -1180,6 +1201,8 @@ class MultiTest(testing_base.Test):
             testcase_report.runtime_status = RuntimeStatus.FINISHED
             if self.get_stdout_style(testcase_report.passed).display_testcase:
                 self.log_testcase_status(testcase_report)
+
+            self.log_testcase_lifecycle(testcase, TestLifecycle.TESTCASE_END)
             return testcase_report
 
         with testcase_report.timer.record("run"):
@@ -1188,8 +1211,14 @@ class MultiTest(testing_base.Test):
                 self.watcher.save_covered_lines_to(testcase_report),
             ):
                 if pre_testcase and callable(pre_testcase):
+                    self.log_testcase_lifecycle(
+                        testcase, TestLifecycle.PRE_TESTCASE_START
+                    )
                     self._run_case_related(
                         pre_testcase, testcase, resources, case_result
+                    )
+                    self.log_testcase_lifecycle(
+                        testcase, TestLifecycle.PRE_TESTCASE_END
                     )
 
                 time_restriction = getattr(testcase, "timeout", None)
@@ -1211,8 +1240,14 @@ class MultiTest(testing_base.Test):
                 self.watcher.save_covered_lines_to(testcase_report),
             ):
                 if post_testcase and callable(post_testcase):
+                    self.log_testcase_lifecycle(
+                        testcase, TestLifecycle.POST_TESTCASE_START
+                    )
                     self._run_case_related(
                         post_testcase, testcase, resources, case_result
+                    )
+                    self.log_testcase_lifecycle(
+                        testcase, TestLifecycle.POST_TESTCASE_END
                     )
 
         # Apply testcase level summarization
@@ -1239,6 +1274,8 @@ class MultiTest(testing_base.Test):
 
         if self.get_stdout_style(testcase_report.passed).display_testcase:
             self.log_testcase_status(testcase_report)
+
+        self.log_testcase_lifecycle(testcase, TestLifecycle.TESTCASE_END)
 
         return testcase_report
 
