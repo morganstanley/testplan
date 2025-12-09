a3a34e70896ce31aaa40bbbd7c538df8f667d289
Yuxuan Cai <Yuxuan.Cai@morganstanley.com>
2025-12-09 13:49:09 +0800

    Update Kafka version.

--- testplan/testing/multitest/driver/kafka.py
+++ testplan/testing/multitest/driver/kafka.py
@@ -12,9 +12,13 @@ from schema import Or
 from testplan.common.config import ConfigOption
 from testplan.common.utils.documentation_helper import emphasized
 from testplan.common.utils.path import instantiate, makedirs, makeemptydirs
+from testplan.common.utils.process import execute_cmd
 from testplan.testing.multitest.driver import app
 
-KAFKA_START = "/opt/kafka/bin/kafka-server-start.sh"
+
+KAFKA_BIN_PATH = "/opt/kafka/bin"
+KAFKA_START = f"{KAFKA_BIN_PATH}/kafka-server-start.sh"
+KAFKA_STORAGE = f"{KAFKA_BIN_PATH}/kafka-storage.sh"
 
 
 class KafkaStandaloneConfig(app.AppConfig):
@@ -27,8 +31,12 @@ class KafkaStandaloneConfig(app.AppConfig):
     def get_options(cls):
         return {
             ConfigOption("cfg_template"): str,
-            ConfigOption("host"): Or(str, None),
+            ConfigOption("host"): str,
             ConfigOption("port"): int,
+            ConfigOption("node_id"): int,
+            ConfigOption("controller_port"): int,
+            ConfigOption("controller_quorum_voters"): Or(str, None),
+            ConfigOption("kafka_storage"): str,
         }
 
 
@@ -55,13 +63,17 @@ class KafkaStandalone(app.App):
         name: str,
         cfg_template: str,
         binary: str = KAFKA_START,
-        host: Optional[str] = None,
+        host: str = "localhost",
         port: int = 0,
+        node_id: int = 1,
+        controller_port: int = 0,
+        controller_quorum_voters: Optional[str] = None,
+        kafka_storage: str = KAFKA_STORAGE,
         **options,
     ):
         stdout_regexps = [
             re.compile(
-                r".*Awaiting socket connections on\s*(?P<host>[^:]+):(?P<port>[0-9]+).*"
+                rf".*Awaiting socket connections on\s*(?P<host>[^:]+):(?P<port>(?!{controller_port}\b)[0-9]+).*"
             ),
             re.compile(".*started.*"),
         ]
@@ -71,7 +83,11 @@ class KafkaStandalone(app.App):
             binary=binary,
             host=host,
             port=port,
+            node_id=node_id,
+            controller_port=controller_port,
             stdout_regexps=stdout_regexps,
+            controller_quorum_voters=controller_quorum_voters,
+            kafka_storage=kafka_storage,
             **options,
         )
 
@@ -80,15 +96,12 @@ class KafkaStandalone(app.App):
         self.config = None
         self._host = host
         self._port = port
+        self._controller_port = controller_port
+        self._controller_quorum_voters = controller_quorum_voters
 
     @emphasized
     @property
     def host(self) -> str:
-        """Host to bind to."""
-        if self._host is None:
-            raise RuntimeError(
-                "Host not resolved yet, shouldn't be accessed now."
-            )
         return self._host
 
     @emphasized
@@ -96,6 +109,27 @@ class KafkaStandalone(app.App):
     def port(self) -> int:
         """Port to listen on."""
         return self._port
+
+    @emphasized
+    @property
+    def node_id(self) -> int:
+        """Node ID."""
+        return self.cfg.node_id
+
+    @emphasized
+    @property
+    def controller_port(self) -> int:
+        """Controller port."""
+        return self._controller_port
+
+    @emphasized
+    @property
+    def controller_quorum_voters(self) -> str:
+        if not self._controller_quorum_voters:
+            self._controller_quorum_voters = (
+                f"{self.cfg.node_id}@{self.host}:{self.controller_port}"
+            )
+        return self._controller_quorum_voters
 
     def pre_start(self):
         super(KafkaStandalone, self).pre_start()
@@ -109,6 +143,22 @@ class KafkaStandalone(app.App):
                 makeemptydirs(directory)
         self.config = os.path.join(self.runpath, "etc", "server.properties")
         instantiate(self.cfg.cfg_template, self.context_input(), self.config)
+        with open(self.config) as config_file:
+            config_data = config_file.read()
+            if "controller.quorum.voter" in config_data:
+                self.format_meta()
+
+    def format_meta(self):
+        cmd = [
+            self.cfg.kafka_storage,
+            "format",
+            "-t",
+            self.name,
+            "-c",
+            self.config,
+            "--ignore-formatted",
+        ]
+        execute_cmd(cmd, env=self.env, label=self.uid(), logger=self.logger)
 
     @property
     def cmd(self) -> List[str]:
