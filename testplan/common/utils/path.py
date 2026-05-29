7e548a65dadaa90009e641dc95e7f87517e6f7ad
Zhenyu Yao <111329301+zhenyu-ms@users.noreply.github.com>
2026-05-29 13:28:36 +0800

    Fix/make `change_directory` thread-safe (#1323)

--- testplan/common/utils/path.py
+++ testplan/common/utils/path.py
@@ -8,6 +8,7 @@ import getpass
 import contextlib
 import tempfile
 import hashlib
+import threading
 from functools import lru_cache
 from typing import Any, Dict, Generator, IO, List, Optional, Set
 
@@ -69,6 +70,7 @@ def default_runpath(entity: Any) -> str:
 
 
 PWD = "PWD"
+CHDIR_LOCK = threading.Lock()
 
 
 @contextlib.contextmanager
@@ -81,22 +83,23 @@ def change_directory(directory: Optional[str]) -> Generator[None, None, None]:
     :type directory: ``str``
     """
 
-    if directory:  # in case we get a None directory, do nothing
-        old_directory = os.getcwd()
-        old_pwd = os.environ.get(PWD, None)
-        directory = fix_home_prefix(directory)
-        os.chdir(directory)
-        if old_pwd:
-            os.environ[PWD] = directory
-    try:
-        yield
-    finally:
-        if directory:
-            os.chdir(old_directory)
+    with CHDIR_LOCK:
+        if directory:  # in case we get a None directory, do nothing
+            old_directory = os.getcwd()
+            old_pwd = os.environ.get(PWD, None)
+            directory = fix_home_prefix(directory)
+            os.chdir(directory)
             if old_pwd:
-                os.environ[PWD] = old_pwd
-            elif PWD in os.environ:
-                del os.environ[PWD]
+                os.environ[PWD] = directory
+        try:
+            yield
+        finally:
+            if directory:
+                os.chdir(old_directory)
+                if old_pwd:
+                    os.environ[PWD] = old_pwd
+                elif PWD in os.environ:
+                    del os.environ[PWD]
 
 
 def makedirs(path: str) -> None:
